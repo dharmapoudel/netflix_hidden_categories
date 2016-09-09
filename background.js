27e88abd44344ec06a79c4b3206ada296f319c6d
Dharma Poudel <dharmapoudel1@gmail.com>
2016-09-08 21:12:49 -0400

    added intital codebase

---
+++ background.js
@@ -0,0 +1,24 @@
+
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains netflix.com
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'netflix.com' },
+            css: ["li.browse"]
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
+
+
+});
+
